dbddb9973a7062902a484d38efccb6e2c63f7a4c
Diego Medeiros <diegoo.fm@gmail.com>
2016-05-03 21:31:03 -0300

    Adds pagination to customer search

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,12 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
     @customers = if params[:keywords].present?
-      Customer.where(search.where_clause, search.where_args).order(search.order)
+      Customer.where(search.where_clause, search.where_args)
+        .order(search.order)
+        .offset(PAGE_SIZE * page)
+        .limit(PAGE_SIZE)
     else
       []
     end
@@ -11,5 +16,9 @@ class CustomersController < ApplicationController
 
   def search
     @_search ||= CustomerSearchTerm.new(params[:keywords])
+  end
+
+  def page
+    @page ||= (params[:page] || 0).to_i
   end
 end
